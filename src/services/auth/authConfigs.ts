bbc186690b90fdd325a678f2350576ddb16fd3fe
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-02-21 23:19:07 +0200

    added msal configs

--- src/services/auth/authConfigs.ts
+++ src/services/auth/authConfigs.ts
@@ -1,9 +1,9 @@
 const msalConfig = {
     auth: {
-        clientId: '',
-        authority: '',
-        knownAuthorities: [''],
-        redirectUri: ''
+        clientId: 'e929e490-e2b3-4c12-8736-7186d0ddd269',
+        authority: 'https://login.microsoftonline.com/common',
+        knownAuthorities: [],
+        redirectUri: 'http://localhost:8080'
     },
     cache: {
         cacheLocation: 'sessionStorage',
