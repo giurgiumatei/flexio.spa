fb14aacd223674160cf72da64cc3566a9861e989
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-02-27 19:11:12 +0200

    updated authority

--- src/services/auth/authConfigs.ts
+++ src/services/auth/authConfigs.ts
@@ -1,8 +1,8 @@
 const msalConfig = {
     auth: {
         clientId: 'e929e490-e2b3-4c12-8736-7186d0ddd269',
-        authority: 'https://flexioorg.b2clogin.com/flexioorg.onmicrosoft.com/B2C_1_SignUpAndSignIn',
-        knownAuthorities: ['https://flexioorg.b2clogin.com/flexioorg.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignUpAndSignIn'],
+        authority: 'https://flexioorg.b2clogin.com/flexioorg.onmicrosoft.com/B2C_1A_PROGRESSIVE_SIGNUP_SIGNIN',
+        knownAuthorities: ['https://flexioorg.b2clogin.com/flexioorg.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_PROGRESSIVE_SIGNUP_SIGNIN'],
         redirectUri: 'http://localhost:8080'
     },
     cache: {
