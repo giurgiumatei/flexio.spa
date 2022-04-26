cd3728ed5cc087b618f28c377f087a61ee9910d7
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-04-27 02:26:11 +0300

    finished UserFeedProfile component

--- src/interfaces/users/userFeedProfileProps.tsx
+++ src/interfaces/users/userFeedProfileProps.tsx
@@ -1,9 +1,6 @@
-import { CommentProps } from '../comments/commentProps';
-
 export interface UserFeedProfileProps {
     id: number,
     displayName: string
     city: string,
     photo: string,
-    comments: CommentProps[]
 }
