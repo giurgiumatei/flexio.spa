603828b2425498dc20b411381ba6d3dabd2026cc
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-04-27 21:53:26 +0300

    added sidebar, started working on feed

--- src/components/layout/Feed.tsx
+++ src/components/layout/Feed.tsx
@@ -1,9 +1,56 @@
-import { Box } from '@mui/material';
+import { Favorite, MoreVert, Share } from '@mui/icons-material';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography
+} from '@mui/material';
 import React from 'react';
+import TestImage from '../../static/assets/images/Gabor.png';
 
 const Feed = () => {
   return (
-    <Box bgcolor={'pink'} flex={4} p={2}>Feed</Box>
+    <Box flex={4} p={2}>
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: 'red' }} aria-label='recipe'>
+              G
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label='settings'>
+              <MoreVert />
+            </IconButton>
+          }
+          title='Andrei Gabor'
+        />
+        <CardMedia
+          component='img'
+          height='5%'
+          image={TestImage}
+          alt='Gabor'
+        />
+        <CardContent>
+          <Typography variant='body2' color='text.secondary'>
+            Suiera trenul in gara
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label='add to favorites'>
+            <Favorite />
+          </IconButton>
+          <IconButton aria-label='share'>
+            <Share />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,15 +1,44 @@
-import { Box } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch
+} from '@mui/material';
 import React from 'react';
+import { Home, ModeNight, Settings } from '@mui/icons-material';
 
 export const Sidebar = () => {
   return (
-    <Box
-      bgcolor={'skyblue'}
-      flex={1}
-      p={2}
-      sx={{ display: { xs: 'none', sm: 'block' } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='home'>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary='Home' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary='Settings' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
