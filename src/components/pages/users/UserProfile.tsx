c97e824340b36073049e58587b382c6d1d7456df
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-05-06 03:21:12 +0300

    solved white bar in dark mode

--- src/components/pages/users/UserProfile.tsx
+++ src/components/pages/users/UserProfile.tsx
@@ -37,7 +37,7 @@ const UserProfile = () => {
       <Card
         sx={{
           margin: 5,
-          width: { xs: '100%', sm: '35%' },
+          width: { xs: '100%', sm: '40%' },
           marginLeft: { sm: '20%' }
         }}
       >
