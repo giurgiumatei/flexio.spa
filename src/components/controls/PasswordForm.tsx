4fc7359514eab02107cb57819ee75178f0cc532d
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-06-01 03:31:27 +0300

    password form done

--- src/components/controls/PasswordForm.tsx
+++ src/components/controls/PasswordForm.tsx
@@ -1,4 +1,11 @@
-import { Input } from '@mui/material';
+import {
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput
+} from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -11,19 +18,52 @@ const schema = yup.object().shape({
   confirmPassword: yup.string().oneOf([yup.ref('password'), null])
 });
 
+interface State {
+  password: string;
+  confirmPassword: string;
+  showPassword: boolean;
+  showConfirmPassword: boolean;
+}
+
 const PasswordForm = () => {
   const {
     register,
     handleSubmit,
-    reset,
-    setValue,
-    getValues,
-    setError,
-    formState: { errors },
-    formState
+    formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
   });
+
+  const [values, setValues] = React.useState<State>({
+    password: '',
+    confirmPassword: '',
+    showPassword: false,
+    showConfirmPassword: false
+  });
+
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword
+    });
+  };
+  const handleClickShowConfirmPassword = () => {
+    setValues({
+      ...values,
+      showConfirmPassword: !values.showConfirmPassword
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
 
   const submitForm = (data) => {
     console.log(data);
@@ -32,20 +72,66 @@ const PasswordForm = () => {
   return (
     <div className='Form'>
       <form onSubmit={handleSubmit(submitForm)}>
-        <input
-          type='password'
-          name='password'
-          placeholder='Password'
-          {...register('password', { required: true })}
-        />
-        <p>{errors.password?.message}</p>
-        <input
-          type='password'
-          name='confirmPassword'
-          placeholder='Confirm Password'
-          {...register('confirmPassword', { required: true })}
-        />
-        <p>{errors.confirmPassword && 'Passwords do not match'}</p>
+        <div>
+          <InputLabel htmlFor='outlined-adornment-password'>
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id='outlined-adornment-password'
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            name='password'
+            placeholder='Password'
+            {...register('password', { required: true })}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton
+                  aria-label='toggle password visibility'
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge='end'
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label='Password'
+          />
+          <p>{errors.password?.message}</p>
+        </div>
+        <div>
+          <InputLabel htmlFor='outlined-adornment-password'>
+            Confirm Password
+          </InputLabel>
+          <OutlinedInput
+            id='outlined-adornment-password'
+            type={values.showConfirmPassword ? 'text' : 'password'}
+            value={values.confirmPassword}
+            name='confirmPassword'
+            placeholder='Confirm Password'
+            {...register('confirmPassword', { required: true })}
+            onChange={handleChange('confirmPassword')}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton
+                  aria-label='toggle password visibility'
+                  onClick={handleClickShowConfirmPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge='end'
+                >
+                  {values.showConfirmPassword ? (
+                    <VisibilityOff />
+                  ) : (
+                    <Visibility />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            }
+            label='Confirm Password'
+          />
+          <p>{errors.confirmPassword && 'Passwords do not match'}</p>
+        </div>
         <input type='submit' id='submit' />
       </form>
     </div>
